a5b1a12693411de76fd7fbc739a2249720755bcc
titusguritno <guritnotitus@gmail.com>
2025-04-28 05:35:18 +0700

    bikin api untuk admin

---
+++ src/app/api/auth/login/route.ts
@@ -0,0 +1,44 @@
+import { NextResponse } from "next/server";
+import prisma from "@/lib/prisma"; // prisma client kamu
+import bcrypt from "bcryptjs";
+
+export async function POST(req: Request) {
+  try {
+    const { username, password } = await req.json();
+
+    if (!username || !password) {
+      return NextResponse.json(
+        { message: "Username dan Password wajib diisi" },
+        { status: 400 }
+      );
+    }
+
+    const user = await prisma.user.findUnique({
+      where: { username },
+    });
+
+    if (!user) {
+      return NextResponse.json(
+        { message: "Akun tidak ditemukan" },
+        { status: 404 }
+      );
+    }
+
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+      return NextResponse.json({ message: "Password salah" }, { status: 401 });
+    }
+
+    return NextResponse.json(
+      { user: { username: user.username, role: user.role } },
+      { status: 200 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { message: "Terjadi kesalahan server" },
+      { status: 500 }
+    );
+  }
+}
